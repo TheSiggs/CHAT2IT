631fffeefd3ff3a3a37ffd2ed7553e4afb1d407e
TheSiggs <samsiggs00@gmail.com>
2023-11-08 17:22:37 +1300

    Heroku deploymenet

--- src/Database/Database.py
+++ src/Database/Database.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 engine = create_engine(
-    os.getenv('DATABASE_URL')
+    os.getenv('EXTERNAL_DB_URL')
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
